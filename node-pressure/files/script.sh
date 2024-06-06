06ffba3b3f5af49e0aa8cb83732ec78f8cd235a4
Lukas Plietl <lukas.plietl@dynatrace.com>
2024-06-06 09:11:32 +0200

    K8S-10127: node-pressure 1.1.0, unification, renaming, updated README

--- node-pressure/files/script.sh
+++ node-pressure/files/script.sh
@@ -7,7 +7,7 @@ apk add curl stress-ng kubectl
 
 if [ "$(kubectl get node "$NODE_NAME" -o jsonpath='{.status.conditions[?(@.type=="MemoryPressure")].status}')" = "False" ]; then 
     echo "No pressure; allocating memory"
-    stress-ng --vm 1 --vm-bytes 5g --vm-hang 0
+    stress-ng --vm 1 --vm-bytes 16g --vm-hang 0
 else 
     echo "Sleeping..."
     sleep 10000d
